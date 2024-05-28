b73f36849a2f0b281952795ac964aab1e9fa7ddc
quanghotrungthinh <quanghotrungthinh@gmail.com>
2024-05-28 11:09:47 +0700

    initial project

---
+++ ProductMiniFullstackApi/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductMiniFullstackApi.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> opts):base(opts)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null);
+                });
+        }
+    }
+}
